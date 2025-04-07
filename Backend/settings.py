d5c0438a911681a2c039e0f4a10e7ee5f270d944
PerfectSoon <totktosdox@gmail.com>
2025-04-07 19:40:13 +0300

    fix settings

--- Backend/settings.py
+++ Backend/settings.py
@@ -1,11 +1,14 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
 
 class Settings(BaseSettings):
     secret_key: str = 'SUPER_SECRET_KEY'
     algorithm: str = 'HS256'
 
-    class Config:
-
-        env_file = ".env"
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore",  # Игнорировать лишние переменные
+    )
 
 settings = Settings()
